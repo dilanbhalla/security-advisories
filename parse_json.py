e438397c81826a23096e82d0267b9250ff15585a
Dilan <dbhalla21@berkeley.edu>
2021-02-14 18:22:40 -0800

    fixed counting issue

--- parse_json.py
+++ parse_json.py
@@ -8,40 +8,26 @@ ecosystem = './ecosystem'
 def retrieve_data(json_data, vuln_file):
 
     vuln = json_data[vuln_file]
-    author_name = vuln['author']['name']
-    author_username = vuln['author']['username']
-    author_website = vuln['author']['website']
-    coordinating_vendor = vuln["coordinating_vendor"][1:] if vuln["coordinating_vendor"] else None
-    created_at = vuln["created_at"]
-    cves = vuln["cves"]
-    cvss_score = vuln["cvss_score"]
-    cvss_vector = vuln["cvss_vector"]
-    id = vuln["id"]
-    module_name = vuln["module_name"]
-    overview = vuln["overview"]
-    patched_versions = vuln["patched_versions"]
-    publish_date = vuln["publish_date"]
-    recommendation = vuln["recommendation"]
-    references = vuln["references"]
-    title = vuln["title"]
-    updated_at = vuln["updated_at"]
-    vulnerable_versions = vuln["vulnerable_versions"]
+
+    (author_name, author_username, author_website, coordinating_vendor, created_at, cves, cvss_score, cvss_vector,
+    id, module_name, overview, patched_versions, publish_date, recommendation, references, title, updated_at, vulnerable_versions) = (vuln['author']['name'],
+    vuln['author']['username'], vuln['author']['website'], vuln["coordinating_vendor"], vuln["created_at"], vuln["cves"],
+    vuln["cvss_score"], vuln["cvss_vector"], vuln["id"], vuln["module_name"], vuln["overview"], vuln["patched_versions"], vuln["publish_date"],
+    vuln["recommendation"], vuln["references"], vuln["title"], vuln["updated_at"], vuln["vulnerable_versions"])
 
     cvss_score = str(cvss_score) if cvss_score else None
     id = str(id) if id else None
 
-    return (author_name, author_username, author_website, coordinating_vendor, created_at,
-    cves, cvss_score, cvss_vector, id, module_name, overview, patched_versions,
-    publish_date, recommendation, references, title, updated_at, vulnerable_versions)
+    return (author_name, author_username, author_website, coordinating_vendor, created_at, cves, cvss_score, cvss_vector, id,
+    module_name, overview, patched_versions, publish_date, recommendation, references, title, updated_at, vulnerable_versions)
 
 def convert_to_markdown(json_data):
 
     num_success, num_failed, succeeded, failed = 0, 0, [], []
     for vuln_file in json_data:
         try:
-            (author_name, author_username, author_website, coordinating_vendor, created_at, cves,
-            cvss_score, cvss_vector, id, module_name, overview, patched_versions, publish_date,
-            recommendation, references, title, updated_at, vulnerable_versions) = retrieve_data(json_data, vuln_file)
+            (author_name, author_username, author_website, coordinating_vendor, created_at, cves, cvss_score, cvss_vector, id, module_name,
+            overview, patched_versions, publish_date,recommendation, references, title, updated_at, vulnerable_versions) = retrieve_data(json_data, vuln_file)
             num_success += 1
             succeeded.append(str(vuln_file))
         except:
@@ -50,44 +36,13 @@ def convert_to_markdown(json_data):
             failed.append(str(vuln_file))
             continue
 
-        # print(author_name)
-        # print(author_username)
-        # print(author_website)
-        # print(coordinating_vendor)
-        # print(created_at)
-        # print(cves)
-        # print(cvss_score)
-        # print(cvss_vector)
-        # print(id)
-        # print(module_name)
-        # print(overview)
-        # print(patched_versions)
-        # print(publish_date)
-        # print(recommendation)
-        # print(references)
-        # print(title)
-        # print(updated_at)
-        # print(vulnerable_versions)
-        # print("\n\n")
+        # print(str(author_name) + "\n" + str(author_username) + "\n" + str(author_website) + "\n" + str(coordinating_vendor) +
+        # "\n" + str(created_at) + "\n" + str(cves) + "\n" + str(cvss_score) + "\n" + str(cvss_vector) + "\n" + str(id) + "\n" + str(module_name) +
+        # "\n" + str(overview) + "\n" + str(patched_versions) + "\n" + str(publish_date) + "\n" + str(recommendation) + "\n" + str(references) +
+        # "\n" + str(title) + "\n" + str(updated_at) + "\n" + str(vulnerable_versions) + "\n")
 
     print("\nCONVERSION: \nSucceeded: " + str(num_success) + ", Failed: " + str(num_failed) + "\n")
 
-# Needs fixing use parse_npm structure to make fixes here
-# def parse_core(path):
-#
-#     data = {}
-#     json_data = json.dumps(data)
-#
-#     for filename in glob.glob(os.path.join(path, '*.json')):
-#         with open(filename, encoding='utf-8', mode='r') as currentFile:
-#             vuln = json.load(currentFile)
-#             data[filename] = vuln
-#             break
-#
-#     json_dump = json.dumps(data, indent = 3).replace("\\n"," ")
-#     json_data = json.loads(json_dump)
-#     print(json_data)
-#     return json_data
 
 def parse_npm(path):
 
@@ -98,7 +53,6 @@ def parse_npm(path):
     start, stop = 1, 10000
 
     num_success, num_failed, succeeded, failed = 0, 0, [], []
-    count = 0
 
     for (dirpath, _, filenames) in os.walk(path):
         for filename in filenames:
@@ -117,8 +71,6 @@ def parse_npm(path):
                     failed.append(filepath)
                     continue
 
-                if filepath in data:
-                    count += 1
                 data[filepath] = vuln
 
                 start += 1
@@ -152,3 +104,23 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+# For core node vulnerabilities. When ready paste this above pare_npm function and fix up (use parse_npm structure to make fixes here)
+# def parse_core(path):
+#
+#     data = {}
+#     json_data = json.dumps(data)
+#
+#     for filename in glob.glob(os.path.join(path, '*.json')):
+#         with open(filename, encoding='utf-8', mode='r') as currentFile:
+#             vuln = json.load(currentFile)
+#             data[filename] = vuln
+#             break
+#
+#     json_dump = json.dumps(data, indent = 3).replace("\\n"," ")
+#     json_data = json.loads(json_dump)
+#     print(json_data)
+#     return json_data
